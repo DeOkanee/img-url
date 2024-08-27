ec9a78ba5d6db4908fddb69f8de5c7be84fb6543
De Oka <115537848+DeOkanee@users.noreply.github.com>
2024-08-27 10:10:11 +0800

    update code

---
+++ public/app.js
@@ -0,0 +1,23 @@
+async function uploadImage() {
+    const formData = new FormData();
+    formData.append('image', document.querySelector('#imageInput').files[0]);
+  
+    try {
+      const response = await fetch('https://img-url-converter.vercel.app/upload', {
+        method: 'POST',
+        body: formData
+      });
+  
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+  
+      const data = await response.json();
+      document.querySelector('#url').textContent = data.url;
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+  
+  document.querySelector('#uploadButton').addEventListener('click', uploadImage);
+  
